01ec90efadbd555ab896c036794f2fb9b7f5890e
mangavenipinninti31-ship-it <mangavenipinninti31@gmail.com>
2026-06-04 12:59:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.getElementById('loginForm').onsubmit = function(e) {
-  e.preventDefault();
-  alert('Login Success Mangaveni!');
-};
+   document.getElementById('loginForm').onsubmit = function(e) {
+     e.preventDefault();
+     alert('Login Success Mangaveni!');
+     window.location.href = 'dashboard.html'; // ee line add chey
+   };
